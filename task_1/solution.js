5b8c651dd0855fc7a40c0e79f4f745cacf416118
DarthAlex <darthalex@mail.ru>
2023-02-16 11:49:46 +0300

    65

--- task_1/solution.js
+++ task_1/solution.js
@@ -56,7 +56,7 @@ function addInCartSumChange(sum, delta) {
     // замените склеивание, используемое в newSum Text на шаблонную строку
     let newSum = oldSum + difference;
     let newSumText = newSum + "₽";
-    let newSumText = "${newSum} ₽";
+    newSumText = "${newSum} ₽";
     console.log (newSum);
 
 
