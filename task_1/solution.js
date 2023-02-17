99a5665ed16cba62c0efa87a623186df874b5f99
DarthAlex <darthalex@mail.ru>
2023-02-17 18:02:50 +0300

    Исправление полное

--- task_1/solution.js
+++ task_1/solution.js
@@ -13,14 +13,14 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
-let message = productName;
-message = message + " ";
-message = message + "За";
-message = message + " ";
-message = message + productPrice;
-message = message + " ";
-message = message + "теперь в корзине";
-console.log (message);
+    let message = productName;
+    message = message + " ";
+    message = message + "за";
+    message = message + " ";
+    message = message + productPrice;
+    message = message + " ";
+    message = message + "теперь в корзине";
+    console.log(message);
 
 
     // Конец решения задания №1.1.
@@ -55,8 +55,9 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
     let newSum = oldSum + difference;
-    let newSumText = newSum + "₽";
-    newSumText = "${newSum}₽";
+    let newSumText = newSum + ' ₽';
+    newSumText = '${newSum} ₽';
+
     console.log (newSumText);
     
 
